7be0c79bb0cb6f4f937e2e11868801f8dd6f9958
Igor Dimitrov <igor-dimi@protonmail.com>
2024-05-31 23:36:26 +0200

    minor change to qsort

--- 05/qsort.cc
+++ 05/qsort.cc
@@ -3,20 +3,22 @@
 void qSort(int* a, int l, int r)
 {
     if (r <= l) return;
-    int p = a[0]; // first element is pivot
+    
+    int p = a[l]; //p is first element
     int i = l;
     int j = r;
     do {
         while (a[i] < p) i++;
         while (a[j] > p) j--;
-        if (i <= j) {// partitioning is not complete 
+        // i < j : partitioning is not yet complete            
+        if (i <= j) {
             int temp = a[i];
             a[i] = a[j];
             a[j] = temp;
             i++;
             j--;
         }
-    } while (i <= j);
+    } while (i <= j) ;
     // i > j
     qSort(a, l, j);
     qSort(a, i, r);
@@ -24,6 +26,12 @@ void qSort(int* a, int l, int r)
 
 int main(int argc, const char** argv) {
     int a[] = {3, 6, 8, 1, 0, 7, 2, 4, 5, 9};
+    int b[] = {3, 2, -1};
+    qSort(b, 0, 2);
+    for (int i = 0; i < 3; i++ )
+        std::cout << b[i] << ", ";
+    std::cout << std::endl;
+    qSort(a, 0, 9);
     for (int i = 0; i < 9; i++)
         std::cout << a[i] << ", "; 
     std::cout << a[9] << std::endl;
